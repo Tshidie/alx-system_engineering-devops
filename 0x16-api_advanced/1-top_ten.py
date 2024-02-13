05965f4fb455ecea4c330ca2506f92c2750f512a
Tshidie <tshidike@icloud.com>
2024-02-13 20:36:52 +0200

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""task 1"""
+"""Contains top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    """Write a function that queries the Reddit API"""
-
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    req = requests.get(url)
-
-    if (req.status_code != 200):
-        print(None)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
         return
-
-    try:
-        childrens = req.json().get('data').get('children')
-        for children in childrens:
-            print(children.get('data').get('title'))
-    except Exception:
-        pass
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
